da8b70fff204cf6155bb13b301a012e272a859af
bedilketek <bedilketek@gmail.com>
2025-07-25 15:34:33 +0700

    Add project files.

---
+++ Core/TestingCookies.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+namespace RomDev.TestingPackageSpace2
+{
+    public class TestingCookies : MonoBehaviour
+    {
+        public void TestPrint()
+        {
+            Debug.Log("Print12");
+        }
+    }
+}
+
